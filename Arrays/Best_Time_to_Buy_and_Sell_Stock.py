be44bf86ef6fe56169e4c21496d3e8d6398185a4
Slayerma <syedmohathashimali93@gmail.com>
2024-07-25 16:04:11 +0530

    Array easy

--- Arrays/Best_Time_to_Buy_and_Sell_Stock.py
+++ Arrays/Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
         min_price, max_profit = float('inf'), 0
         for price in prices:
             min_price = min(min_price, price)
