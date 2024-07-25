5f8ebebadf02aa51f602eac44056162e65946290
Slayerma <syedmohathashimali93@gmail.com>
2024-07-25 14:56:01 +0530

    Array easy

---
+++ Arrays/Two_Sums.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = set()
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [nums.index(complement), i]
+            seen.add(num)
+        return []
