ae540fc526317d14c15eb275dabb641700a133e0
Slayerma <syedmohathashimali93@gmail.com>
2024-07-30 23:55:51 +0530

    Array Meduim

---
+++ Arrays/Maximum_Points_You_Can_Obtain_from_Cards.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxScore(self, cardPoints: list[int], k: int) -> int: 
+         # Use two pointers to calculate the score by taking cards from the end
+        n = len(cardPoints)
+        
+        # Initial score using the first k cards
+        current_score = sum(cardPoints[:k])
+        max_score = current_score
+        
+        # Use a sliding window to calculate the score by taking cards from the end
+        for i in range(1, k + 1):
+            current_score += cardPoints[-i] - cardPoints[k - i]
+            max_score = max(max_score, current_score)
+        
+        return max_score
